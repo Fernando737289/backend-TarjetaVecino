4f27b001e3416adcfa539dd5b7414120c4d64aee
Francisco-24101981 <francisco.baez.24.10.81@gmail.com>
2026-05-16 21:07:07 -0400

    conexion a base de dato lista (#3)

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
-from app.routers import users
+from app.routers import db_conection, users
 
-app = FastAPI(title="Mi API de Usuarios")
+
+app = FastAPI(title="Mi API")
 
 app.include_router(users.router)
+app.include_router(db_conection.router)
 
 @app.get("/")
 def read_root():
