8f7e737f42eac2677c8ec0e89e5708853349f3b8
hernaldo2025 <hernaldomontecinos1@gmail.com>
2026-05-18 19:24:01 -0400

    Test/documentacion (#9)
    * avances en el crud persona (#5)
    
    * Develop (#6)
    
    * avances en el crud persona
    
    * actualizacion del readme
    
    * Develop (#8)
    
    * avances en el crud persona
    
    * actualizacion del readme
    
    * Feature/sql (#7)
    
    * avances en el crud persona (#5)
    
    * Develop (#6)
    
    * avances en el crud persona
    
    * actualizacion del readme
    
    * se le agrego el punto sql al proyecto
    
    ---------
    
    Co-authored-by: Fernando737289 <148507001+Fernando737289@users.noreply.github.com>
    Co-authored-by: Alumnos República - ALUMNOS <alumnos.republica@alumnos.local>
    
    ---------
    
    Co-authored-by: Josemunoz9727 <munozlopezjose97@gmail.com>
    Co-authored-by: Alumnos República - ALUMNOS <alumnos.republica@alumnos.local>
    
    * documentacion de las apis
    
    ---------
    
    Co-authored-by: Fernando737289 <148507001+Fernando737289@users.noreply.github.com>
    Co-authored-by: Josemunoz9727 <munozlopezjose97@gmail.com>
    Co-authored-by: Alumnos República - ALUMNOS <alumnos.republica@alumnos.local>

--- app/routers/db_conection.py
+++ app/routers/db_conection.py
@@ -3,6 +3,7 @@ from app.services.db_service import database_connection
 
 router = APIRouter(prefix="/health", tags=["Health"])
 
+#ruta de prueba para probar conexion a base de datos
 @router.get("/test-db")
 def db():
     return database_connection()
