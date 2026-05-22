86903cc7d38296b4a1fa6243a5aa4e94892a321c
Fernando Maturana <fernando.maturana.hidalgo@gmail.com>
2026-05-22 01:32:22 -0400

    se agregaron los corsMiddleware

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
+
 from app.routers import db_conection, users, qr_router, tarjeta_router
+
+from app.routers import db_conection, users
+
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Mi API")
@@ -8,6 +12,14 @@ app.include_router(users.router)
 app.include_router(db_conection.router)
 app.include_router(qr_router.router)
 app.include_router(tarjeta_router.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.add_middleware(
     CORSMiddleware,
